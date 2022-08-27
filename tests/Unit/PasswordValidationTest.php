91193114d845020b497b7be4dcd726529afcffbd
SeyedHadi Hosseini <hadi.hosseini88@gmail.com>
2022-08-27 08:45:04 +0430

    test for password validtion is compelet

--- tests/Unit/PasswordValidationTest.php
+++ tests/Unit/PasswordValidationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use Hadihosseini88\User\Rules\ValidPassword;
 use PHPUnit\Framework\TestCase;
 
 class PasswordValidationTest extends TestCase
@@ -11,8 +12,31 @@ class PasswordValidationTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function test_password_should_not_be_less_than_6_character()
     {
-        $this->assertTrue(true);
+        $result = (new ValidPassword())->passes('', 'Aa@12');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_password_should_include_sign_character()
+    {
+        $result = (new ValidPassword())->passes('', 'Aa12345');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_password_should_include_digit_character()
+    {
+        $result = (new ValidPassword())->passes('', 'Aa@AAAAA');
+        $this->assertEquals(0, $result);
+    }
+    public function test_password_should_include_capital_character()
+    {
+        $result = (new ValidPassword())->passes('', 'aa@12345');
+        $this->assertEquals(0, $result);
+    }
+    public function test_password_should_include_small_character()
+    {
+        $result = (new ValidPassword())->passes('', 'AA@12345');
+        $this->assertEquals(0, $result);
     }
 }
